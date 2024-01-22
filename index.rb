1859bd2e13069ac029e9d78d3b798d0c0bd3962b
Augusto Faggion <afaggionliradossantos@gmail.com>
2024-01-22 23:33:55 +0100

    modifiy

--- index.rb
+++ index.rb
@@ -12,12 +12,11 @@
 
 # Take your time to write the code, and feel free to ask for help or clarification if needed. Once you've written your code, you can share it with me, and I'll provide feedback!
 
-
 def add(num1, num2)
   num1 + num2
 end
 
-def subtract(num1, num2)
+def sub(num1, num2)
   num2 - num1
 end
 
@@ -26,30 +25,33 @@ def multiply(num1, num2)
 end
 
 def divide(num1, num2)
-  num2 / num2
+  num2 / num1
 end
 
-puts "First number?"
+puts "What is your first number?"
 num1 = gets.chomp.to_f
 
-puts "Operator"
+puts "Choose an operator for the Calculator"
 operator = gets.chomp
 
-puts "Second nummer?"
+puts "What is your second number?"
 num2 = gets.chomp.to_f
 
-result =
+result = 
 case operator
-  when "+"
-    add(num1, num2)
-  when "-"
-    subtract(num1, num2)
-  when "*"
-    multiply(num1,num2)
-  when "/"
-    divide(num1,num2)
-  else
-    "Invalid operator"
-  end
+when "+"
+  add(num1,num2)
+when "-"
+  sub(num1,num2)
+when "*"
+  multiply(num1, num2)
+when "/"
+  divide(num1, num2)
+else
+  "Error"
+end
+
+
+
 
 
